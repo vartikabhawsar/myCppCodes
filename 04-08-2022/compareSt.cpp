7c88de9432eaf284883b01b3ffec431c15938942
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-08-07 23:21:15 +0530

    Add files via upload

---
+++ 04-08-2022/compareSt.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    
+    int c=0,i,d=0,temp;
+    char a[30],b[30];
+    cout<<"enter the string :";
+    cin.getline(a,30);
+    cout<<"enter the next string :";
+    cin.getline(b,30);
+    c=strlen(a);//c has length of a without using loop :)
+    while(a[d]!='\0')
+    {
+        d++;
+    }
+    if(d==c)
+    {
+        for(i=0;i<c;i++)
+        {
+            if(a[i]==b[i]);
+            else
+            break;
+
+        }
+        if(i==c)
+        {
+            cout<<"equal string";
+        }
+        else 
+        {
+        cout<<"unequal";
+        }
+    }
+    else cout<<"nt equal";
+
+}
