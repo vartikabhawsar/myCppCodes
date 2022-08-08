0598adfa5dc6fed78261053398337a362f4b8aaa
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-08-08 23:27:17 +0530

    Add files via upload

--- 07-08-2022/countHowManyTimesaWordOccur.cpp
+++ 07-08-2022/countHowManyTimesaWordOccur.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     
-    int count=0,j=0;
+    int count=0,j=0,k;
     char a[100],w[30];
     cout<<"enter the string :";
     cin.getline(a,30);
@@ -12,26 +12,37 @@ int main()
     cout<<"enter the word to be searched :";
     cin.getline(w,30);
     int d=strlen(w);
-
+for(k =0;k<d;k++)
+{
+    if(a[k]!=w[k])
+    {
+       break;
+    }
+}
+if(k==d)
+{
+    count++;
+}
 for(int i=0;i<c;i++)
 {
-      
+    if(a[i]==' ')
+    {
     for(j=0;j<d;j++)
     {
-        if(a[i+j]!=w[j])
+        if(a[i+1+j]!=w[j])
         {
             break;
         }
     }
         if(j==d)
         {
-           cout<<"subtring found";
-        }
-        else
-        {
-            cout<<"substring not found";
+            if( i+j+1==c || a[i+j+1]==' ')
+            {
+            count++;
+            }
         }
 
 }
-
+}
+cout<<count;
 }
