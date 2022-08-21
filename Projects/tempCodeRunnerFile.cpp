e0a2f14bae80f51319028f650545aea69b16d06e
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-08-21 18:59:32 +0530

    Add files via upload

---
+++ Projects/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+            else if(command=="open youtube")
+                {
+                    cout<<"openining YouTube"<<endl;
+                    string phrase = "opening youtube";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    system("start https://www.youtube.com");
+                }
