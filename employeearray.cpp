5c503d422b472261b06707978c3e400bfe6c1fdc
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-11-03 17:47:53 +0530

    Add files via upload

---
+++ employeearray.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+class employee
+{
+    char name[20];
+    float eno;
+    float basic;
+    float hra;
+    float cca;
+    float da;
+    float pf;
+    float netsal;
+    public:
+    void input()
+    {
+        cout<<"enter name eno and basic ";
+        cin>>name;
+        cin>>eno;
+        cin>>basic;
+    }
+    void calculate()
+    {
+        hra=(40.0/100)*basic;
+        da=(35.0/100)*basic;
+        cca=(5.0/100)*basic;
+        pf=(12.0/100)*basic;
+        netsal = basic + hra + da + cca - pf;
+
+    }
+    void output()
+    {
+        
+        cout<<name<<" ";
+        cout<<eno<<" ";
+        cout<<netsal<<" ";
+
+    }
+
+};
+int main()
+{
+    employee e1[3];
+    for(int i=0;i<3;i++)
+    {
+    e1[i].input();
+    e1[i].calculate();
+    e1[i].output();
+    }
+}
