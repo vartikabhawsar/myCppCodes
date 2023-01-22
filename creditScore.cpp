f9d214bcdbb819ea956200e4cd6c6a9549f684a0
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2023-01-22 23:13:00 +0530

    Add files via upload

---
+++ creditScore.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	if(x>=750)
+	{
+	    cout<<"yes";
+	}
+	else
+	{
+	    cout<<"no";
+	}
+	// your code goes here
+	return 0;
+}
