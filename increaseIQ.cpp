368097879425ce16746d418078312bbf0b12e579
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2023-02-07 23:56:45 +0530

    Add files via upload

---
+++ increaseIQ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    if(x+7>170)
+    cout<<"yes"<<endl;
+    else
+    cout<<"no"<<endl;
+	// your code goes here
+	return 0;
+}
