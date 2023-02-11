eb77fb6bb5e70f729cc51141f4f564db16765512
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2023-02-11 23:56:13 +0530

    Add files via upload

---
+++ pracicemakesusperfect.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int p1,p2,p3,p4;
+	cin>>p1>>p2>>p3>>p4;
+	int c=0;
+	if(p1>10)
+	c++;
+	if(p2>=10)
+	c++;
+	if(p3>=10)
+	c++;
+	if(p4>=10)
+	c++;
+	
+	cout<<c;
+	return 0;
+}
