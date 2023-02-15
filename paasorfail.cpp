7fc7ef6257402314e45b97a0e7cf3ad447a5c5a4
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2023-02-15 23:58:21 +0530

    Add files via upload

---
+++ paasorfail.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x,p;
+        cin>>n>>x>>p;
+        if((x*3)-(n-x)>=p)
+        cout<<"pass"<<endl;
+        else
+         cout<<"fail"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
